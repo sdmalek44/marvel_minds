3d7ae8d0f610b53a6586a42af7dfc2d8b509a885
Stephen Malek <malek.stephen44@gmail.com>
2018-11-16 17:21:05 -0700

    Refactor series show presenter, pull methods into service

--- app/services/marvel_service.rb
+++ app/services/marvel_service.rb
@@ -12,6 +12,18 @@ class MarvelService
     @char_data ||= get_json("/v1/public/characters/#{character_id}?ts=1&apikey=1d68fdb48109bd44fd202dd9c0df3866&hash=108c35713f83d455e1a871dd3858751d")[0]
   end
 
+  def series_data(series_id)
+    @series_data ||= get_json("/v1/public/series/#{series_id}?ts=1&apikey=1d68fdb48109bd44fd202dd9c0df3866&hash=108c35713f83d455e1a871dd3858751d")[0]
+  end
+
+  def series_events_data(series_id)
+    @series_events ||= get_json("/v1/public/series/#{series_id}/events?ts=1&apikey=1d68fdb48109bd44fd202dd9c0df3866&hash=108c35713f83d455e1a871dd3858751d")
+  end
+
+  def series_char_data(series_id)
+    @series_series ||= get_json("/v1/public/series/#{series_id}/characters?ts=1&apikey=1d68fdb48109bd44fd202dd9c0df3866&hash=108c35713f83d455e1a871dd3858751d")
+  end
+
   def char_series_data(character_id)
     @char_series ||= get_json("/v1/public/characters/#{character_id}/series?ts=1&apikey=1d68fdb48109bd44fd202dd9c0df3866&hash=108c35713f83d455e1a871dd3858751d")
   end
